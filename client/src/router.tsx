40b1766d44a69b303a14646072d8a33f61a8496b
Homvi <honvedoadamkj@gmail.com>
2024-09-18 15:36:58 +0200

    feat(router): update routes and layouts for public and protected pages

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,39 @@
-// client/src/router.tsx
+// src/router.tsx
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+import Landing from './Pages/Landing';
 import Home from './Pages/Home';
-import { createBrowserRouter } from 'react-router-dom';
+import Login from './Pages/Login';
+import { PublicLayout } from './Layouts/PublicLayout';
+import { ProtectedLayout } from './Layouts/ProtectedLayout';
+import { AuthLayout } from './Layouts/AuthLayout';
 
-export const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<AuthLayout />}>
+        <Route element={<PublicLayout />}>
+          <Route
+            path="/"
+            element={<Landing />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+        </Route>
+
+        <Route element={<ProtectedLayout />}>
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+        </Route>
+      </Route>
+    </>
+  )
+);
