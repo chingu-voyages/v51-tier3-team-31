f7062db2bef30488c526656ca14916ea610f49a1
Homvi <honvedoadamkj@gmail.com>
2024-09-25 19:19:31 +0200

    feat(NewExpenseGroupFormModal): added support to refresh expense groups after creation
    - Passed getExpenseGroups function as a prop to refresh expense groups after creating a new one.
    - Added logic to clear the form after successful submission and fetch the updated expense groups.

--- client/src/components/NewExpenseGroupsFormModal.tsx
+++ client/src/components/NewExpenseGroupsFormModal.tsx
@@ -1,63 +1,97 @@
-import axios from "axios";
-import React from "react";
+import axios from 'axios';
+import { useAuth } from '../hooks/useAuth';
+import { useEffect, useState } from 'react';
+import { serverBaseUrl } from '../config';
 
-export default function NewExpenseGroupFormModal() {
-    
-    //Need to be moved somewhere else
-    const serverBaseUrl = import.meta.env.VITE_SERVER_BASE_URL;
-    const url = `${serverBaseUrl}/api/v1/expense-groups`;
+interface NewExpenseGroupFormModalProps {
+  getExpenseGroups: () => void;
+}
 
-    const [formData, setFormData] = React.useState(
-    {name: "", description: "", budget: 0}
-    )
-    //Need to find type for event
-    function handleChange(event :any) {
-    setFormData(prevFormData => {
-        return {
-            ...prevFormData,
-            [event.target.name]: event.target.value
-        }
-    })
+export default function NewExpenseGroupFormModal({
+  getExpenseGroups,
+}: NewExpenseGroupFormModalProps) {
+  const url = `${serverBaseUrl}/api/v1/expense-groups`;
+
+  const { user } = useAuth();
+
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    budget: 0,
+    createdBy: 0,
+  });
+
+  function handleChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    axios
+      .post(url, formData)
+      .then((res) => {
+        console.log('Success!', res);
+
+        // clear input fields
+        setFormData({
+          name: '',
+          description: '',
+          budget: 0,
+          createdBy: 0,
+        });
+
+        getExpenseGroups();
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  // Update formData on mount to set createdBy to user.id
+  useEffect(() => {
+    if (user?.id) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        createdBy: Number(user.id),
+      }));
     }
+  }, [user?.id]);
 
-    function handleSubmit(event :any) {
-        event.preventDefault()
-        submitToApi()
-    }
-
-    function submitToApi() {
-        axios
-            .post(url, formData)
-            .then((res) => {
-            console.log('Success!', res)
-            })
-            .catch((error) => {
-            console.error('Error fetching data:', error);
-            });
-    }
-
-    return (
-            <div className="fixed bottom-20">
-                <div className="bg-gray-300 h-[200px] w-[290px] border border-indigo-900 shadow-md shadow-gray-600 relative cursor-pointer">
-                    <form onSubmit={handleSubmit} className="p-3 flex flex-col">
-                        <label htmlFor="name">Account name</label>
-                        <input
-                            type="text"
-                            placeholder="Name"
-                            onChange={handleChange}
-                            name="name"
-                            id="name"
-                        />
-                        <label htmlFor="isFriendly">Description</label>
-                        <textarea
-                            placeholder="Description"
-                            onChange={handleChange}
-                            name="description"
-                            id="description"
-                        />
-                        <button className="bg-indigo-900 text-gray-50 mt-2">Add</button>
-                    </form>
-                </div>
-            </div>     
-    );
+  return (
+    <div className="fixed bottom-20">
+      <div className="bg-gray-300 h-[200px] w-[290px] border border-indigo-900 shadow-md shadow-gray-600 relative cursor-pointer">
+        <form
+          onSubmit={handleSubmit}
+          className="p-3 flex flex-col"
+        >
+          <label htmlFor="name">Expense group name</label>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={handleChange}
+            value={formData.name}
+            name="name"
+            id="name"
+          />
+          <label htmlFor="isFriendly">Description</label>
+          <textarea
+            placeholder="Description"
+            value={formData.description}
+            onChange={handleChange}
+            name="description"
+            id="description"
+          />
+          <button className="bg-indigo-900 text-gray-50 mt-2">Add</button>
+        </form>
+      </div>
+    </div>
+  );
 }
