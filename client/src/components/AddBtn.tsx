291e7b8dde2f5c62572627b240390f72c309a22e
Homvi <honvedoadamkj@gmail.com>
2024-09-26 15:45:16 +0200

    feat(AddBtn): replace custom add button with lucide-react CirclePlus icon

--- client/src/components/AddBtn.tsx
+++ client/src/components/AddBtn.tsx
@@ -1,3 +1,5 @@
+import { CirclePlus } from 'lucide-react';
+
 interface AddBtnProps {
   toggleModal: () => void;
 }
@@ -8,11 +10,11 @@ export default function AddBtn({ toggleModal }: AddBtnProps) {
       onClick={toggleModal}
       className="flex flex-col items-center"
     >
-      <div className="bg-gray-300 size-14 rounded-full border border-indigo-900 shadow-md shadow-gray-600 relative cursor-pointer">
-        <div className="bg-indigo-900 w-1 h-9 absolute top-2.5 left-[25px] rounded-md"></div>
-        <div className="bg-indigo-900 h-1 w-9 absolute left-2.5 top-[25px] rounded-md"></div>
-      </div>
-      <p className="text-gray-50">Add an expense group</p>
+      <CirclePlus
+        size={42}
+        className="cursor-pointer text-neutral-800 hover:text-black"
+      />
+      <p className="text-neutral-800">Add an expense group</p>
     </div>
   );
 }
