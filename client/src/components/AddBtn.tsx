cecba525d69c2b8d82216d4de2a4c145ffa4c0e9
Homvi <honvedoadamkj@gmail.com>
2024-09-26 11:09:11 +0200

    feat(add-btn): update button to toggle modal visibility
    - Accept toggleModal prop to handle modal state.
    - Adjust click handling to toggle modal on button click.

--- client/src/components/AddBtn.tsx
+++ client/src/components/AddBtn.tsx
@@ -1,6 +1,13 @@
-export default function AddBtn() {
+interface AddBtnProps {
+  toggleModal: () => void;
+}
+
+export default function AddBtn({ toggleModal }: AddBtnProps) {
   return (
-    <div className="fixed bottom-3 flex flex-col items-center">
+    <div
+      onClick={toggleModal}
+      className="flex flex-col items-center"
+    >
       <div className="bg-gray-300 size-14 rounded-full border border-indigo-900 shadow-md shadow-gray-600 relative cursor-pointer">
         <div className="bg-indigo-900 w-1 h-9 absolute top-2.5 left-[25px] rounded-md"></div>
         <div className="bg-indigo-900 h-1 w-9 absolute left-2.5 top-[25px] rounded-md"></div>
