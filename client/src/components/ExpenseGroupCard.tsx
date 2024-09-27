16cce05d8886b1eb34478412b9f6e41fe203a3d4
Homvi <honvedoadamkj@gmail.com>
2024-09-27 11:16:27 +0200

    feat: enhance ExpenseGroupCard with participant badges
    - Updated the ExpenseGroupCard component to display user participants as badges.
    - Imported UserExpenseGroup type and Badge component for better participant representation.
    - Added a ChevronRight icon for improved navigation interaction.
    - Adjusted layout for better visual structure.

--- client/src/components/ExpenseGroupCard.tsx
+++ client/src/components/ExpenseGroupCard.tsx
@@ -1,23 +1,36 @@
-interface ExpenseGroup {
-  id: number;
-  name: string;
-  userExpenseGroups: [];
-}
+import { ExpenseGroup, UserExpenseGroup } from '../types/expenseGroup';
+import { ChevronRight } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
 
 interface ExpenseGroupCardProps {
   expenseGroup: ExpenseGroup;
 }
 
 const ExpenseGroupCard = ({ expenseGroup }: ExpenseGroupCardProps) => {
+  const participants: UserExpenseGroup[] = expenseGroup.userExpenseGroups;
+
   return (
-    <div
-      key={expenseGroup.id}
-      className="py-3 px-3 flex gap-1 flex-col w-[290px] shadow-md cursor-pointer text-neutral-800 rounded-md justify-center items-center"
-    >
-      <p className="text-lg text-left w-full">{expenseGroup.name}</p>
-      <p className="text-left w-full">
-        Number of participants: {expenseGroup.userExpenseGroups.length}
-      </p>
+    <div className="flex items-stretch h-full">
+      <div
+        key={expenseGroup.id}
+        className="py-3 text-left px-3 flex gap-1 flex-col w-[290px] shadow-md cursor-pointer text-neutral-800 rounded-l-md justify-center items-center"
+      >
+        <p className="text-lg  w-full">{expenseGroup.name}</p>
+        <p className="w-full">Participants: </p>
+        <div className="flex w-full flex-wrap gap-1">
+          {participants.map((participant) => (
+            <Badge
+              className="bg-black/50"
+              key={participant.id}
+            >
+              {participant.user.name}
+            </Badge>
+          ))}
+        </div>
+      </div>
+      <div className="min-w-11 cursor-pointer hover:bg-gray-200 shadow-md  bg-gray-100 rounded-r-md px-2 flex flex-col items-center justify-center">
+        <ChevronRight />
+      </div>
     </div>
   );
 };
