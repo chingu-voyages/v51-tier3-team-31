87446cad05a690f0ae99f7db3f4639be7e62cdd7
Homvi <honvedoadamkj@gmail.com>
2024-09-30 13:14:38 +0200

    refactor(navbar): simplify dropdown menu item rendering logic

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -16,41 +16,40 @@ const Navbar = ({ isLoggedIn }: NavbarProps) => {
   const { logout } = useAuth();
 
   return (
-    <>
-      <nav className=" text-primary border-b-[1px] py-2 flex  justify-between items-center px-3 backdrop-blur-xl sticky top-0 w-full">
-        <Link to="/">
-          <div className="py-1 px-3 rounded-l-sm border-r-2 font-bold">
-            <span className="font-bold text-xl text-blue-600">/</span> Splitit
-          </div>
-        </Link>
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Menu size={26} className="rotate-90" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="mx-3">
-            {isLoggedIn && (
-              <Link to="/home">
-                <DropdownMenuItem>Dasboard</DropdownMenuItem>
-              </Link>
-            )}
-            {!isLoggedIn && (
-              <Link to="/login">
-                <DropdownMenuItem>Login</DropdownMenuItem>
-              </Link>
-            )}
-            {!isLoggedIn && (
-              <Link to="/login">
-                <DropdownMenuItem>Register</DropdownMenuItem>
-              </Link>
-            )}
-            {/*  <DropdownMenuSeparator /> */}
-            {isLoggedIn && (
+    <nav className="text-primary border-b-[1px] py-2 flex justify-between items-center px-3 backdrop-blur-xl sticky top-0 w-full">
+      <Link to="/">
+        <div className="py-1 px-3 rounded-l-sm border-r-2 font-bold">
+          <span className="font-bold text-xl text-blue-600">/</span> Splitit
+        </div>
+      </Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Menu
+            size={26}
+            className="rotate-90"
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mx-3">
+          {isLoggedIn ? (
+            <>
+              <DropdownMenuItem asChild>
+                <Link to="/home">Dashboard</Link>
+              </DropdownMenuItem>
               <DropdownMenuItem onClick={logout}>Logout</DropdownMenuItem>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </nav>
-    </>
+            </>
+          ) : (
+            <>
+              <DropdownMenuItem asChild>
+                <Link to="/login">Login</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link to="/register">Register</Link>
+              </DropdownMenuItem>
+            </>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
   );
 };
 
