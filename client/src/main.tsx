fde32884071744f1669de21a46e522646da3c913
Homvi <honvedoadamkj@gmail.com>
2024-10-04 13:45:15 +0200

    feat: add react-query client provider to the application
    - Wrapped the RouterProvider with QueryClientProvider.
    - Initialized QueryClient to manage server state in the application.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { router } from './router.tsx';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
