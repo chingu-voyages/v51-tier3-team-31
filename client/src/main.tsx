400e465795b557f51f3447494d7e0cbade3ecad8
Homvi <honvedoadamkj@gmail.com>
2024-10-07 08:10:10 +0200

    remove tanstack/react-query-devtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,6 @@ import { router } from './router.tsx';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -12,7 +11,6 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
