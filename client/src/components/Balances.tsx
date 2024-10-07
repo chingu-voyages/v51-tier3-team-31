c1cc87bc03401609e558be04622957ba558cfff0
Homvi <honvedoadamkj@gmail.com>
2024-10-07 16:28:05 +0200

    feat: implement Balances component with balance fetching and rendering

--- client/src/components/Balances.tsx
+++ client/src/components/Balances.tsx
@@ -1,7 +1,39 @@
-const Balances = () => {
-  return (
-    <div>Balances</div>
-  )
+import useBalances from '@/hooks/useBalances';
+import { Balance } from '@/types/balance';
+import { useEffect, useState } from 'react';
+import BalanceCard from './BalanceCard';
+
+interface BalancesPropsType {
+  expenseGroupId: number | undefined;
 }
 
-export default Balances
+const Balances = ({ expenseGroupId }: BalancesPropsType) => {
+  const [balances, setBalances] = useState<Balance[]>([]);
+  const expenseGroupIdString = expenseGroupId?.toString();
+  const { data } = useBalances(expenseGroupIdString!);
+
+  useEffect(() => {
+    if (data?.balances) {
+      setBalances(data?.balances);
+    }
+  }, [data]);
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-lg">Total expense: {data?.totalExpenses} $</div>
+      </div>
+      <div className="flex flex-col gap-3 mt-5">
+        {balances.map((balance) => (
+          <BalanceCard
+            key={balance.userId}
+            email={balance.userEmail}
+            balance={balance}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Balances;
