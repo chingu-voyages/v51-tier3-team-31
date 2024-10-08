f9a7bd8bb97d6c022021d111ee1230076f3f665d
Homvi <honvedoadamkj@gmail.com>
2024-10-08 08:59:49 +0200

    Clarify data structure in Balances component by renaming variable to balancesObj

--- client/src/components/Balances.tsx
+++ client/src/components/Balances.tsx
@@ -10,18 +10,19 @@ interface BalancesPropsType {
 const Balances = ({ expenseGroupId }: BalancesPropsType) => {
   const [balances, setBalances] = useState<Balance[]>([]);
   const expenseGroupIdString = expenseGroupId?.toString();
-  const { data } = useBalances(expenseGroupIdString!);
+  const { data: balancesObj } = useBalances(expenseGroupIdString!);
 
+  // set balances to balances array that is a property on balances object
   useEffect(() => {
-    if (data?.balances) {
-      setBalances(data?.balances);
+    if (balancesObj?.balances) {
+      setBalances(balancesObj?.balances);
     }
-  }, [data]);
+  }, [balancesObj]);
 
   return (
     <>
       <div className="flex flex-col justify-center items-center">
-        <div className="text-lg">Total expense: {data?.totalExpenses} $</div>
+        <div className="text-lg">Total expense: {balancesObj?.totalExpenses} $</div>
       </div>
       <div className="flex flex-col gap-3 mt-5">
         {balances.map((balance) => (
