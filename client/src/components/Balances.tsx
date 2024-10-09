e8f24a6c3a4b4c02a244a97bbc4413aefd626cee
Homvi <honvedoadamkj@gmail.com>
2024-10-09 08:50:57 +0200

    show payment info

--- client/src/components/Balances.tsx
+++ client/src/components/Balances.tsx
@@ -1,9 +1,13 @@
-import useBalances from '@/hooks/useBalances';
-import { Balance } from '@/types/balance';
 import { useEffect, useState } from 'react';
-import BalanceCard from './BalanceCard';
-import { Receipt } from 'lucide-react';
+import useBalances from '@/hooks/useBalances';
+import usePayments from '@/hooks/usePayments';
 import { Card, CardContent } from './ui/card';
+import BalanceCard from './BalanceCard';
+import PaymentCard from './PaymentCard';
+import { ThumbsUp, Receipt } from 'lucide-react';
+import { Balance } from '@/types/balance';
+import { Payment } from '@/types/payment';
+import { User } from '@/types/user';
 
 interface BalancesPropsType {
   expenseGroupId: number | undefined;
@@ -11,41 +15,107 @@ interface BalancesPropsType {
 
 const Balances = ({ expenseGroupId }: BalancesPropsType) => {
   const [balances, setBalances] = useState<Balance[]>([]);
+  const [payments, setPayments] = useState<Payment[]>([]);
+  const [needsBalancing, setNeedsBalancing] = useState<Boolean>(false);
+  const [paymentsOfCurrentUser, setPaymentsOfCurrentUser] = useState<Payment[]>(
+    []
+  );
   const expenseGroupIdString = expenseGroupId?.toString();
   const { data: balancesObj } = useBalances(expenseGroupIdString!);
+  const { data: expenseGroupPayments } = usePayments(expenseGroupIdString!);
 
-  // set balances to balances array that is a property on balances object
+  // Temporary mock for user authentication
+  const user: User = {
+    id: 2,
+    googleId: '0',
+    name: 'Béla',
+    email: 'asdsda@asasd.com',
+    createdAt: '1',
+    updatedAt: '1',
+  };
+
+  // Set balances and payments when the data is fetched
   useEffect(() => {
     if (balancesObj?.balances) {
       setBalances(balancesObj?.balances);
     }
-  }, [balancesObj]);
+    if (expenseGroupPayments?.payments) {
+      setPayments(expenseGroupPayments?.payments);
+    }
+  }, [balancesObj, expenseGroupPayments]);
+
+  // Filter payments by the current user's ID whenever payments or user change
+  useEffect(() => {
+    if (user?.id && payments) {
+      const filteredPayments = payments.filter((payment) => {
+        return (
+          payment.fromUserId === Number(user.id) ||
+          payment.toUserId === Number(user.id)
+        );
+      });
+      setPaymentsOfCurrentUser(filteredPayments);
+    }
+  }, []);
+
+  useEffect(() => {
+    const needBalancing = paymentsOfCurrentUser.length > 0;
+    setNeedsBalancing(needBalancing);
+  }, [paymentsOfCurrentUser, user.id]);
+
+  const generateKey = (pre: any) => {
+    return `${pre}_${new Date().getTime()}`;
+  };
 
   return (
     <div>
-      <Card className="max-w-md bg-gradient-to-br  w-80 rounded-md mx-auto ">
-        <CardContent className="py-3">
-          <div className="flex items-center justify-between space-x-4">
-            <div className="p-3  text-slate-600">
-              <Receipt size={35}  />
-            </div>
-            <div className="text-center">
-              <p className="text-sm text-gray-500 font-medium">Total Expense</p>
-              <p className="text-xl font-semibold text-gray-700">
-                {balancesObj?.totalExpenses?.toFixed(2) ?? '0.00'} €
-              </p>
-            </div>
-          </div>
-        </CardContent>
-      </Card>
-      <div className="flex flex-col gap-3 mt-5">
+      <div className="flex flex-col gap-3 w-80 mx-auto mt-5">
+        {/* Payment card */}
+        {!needsBalancing && (
+          <Card>
+            <CardContent className="flex justify-between rounded-md w-full items-center p-2">
+              <div className="flex w-1/3 justify-center items-center text-slate-600">
+                <ThumbsUp size={35} />
+              </div>
+              <div className="flex w-full flex-col">
+                <h3 className="font-semibold">All good</h3>
+                <p className="text-sm">You don't need to balance</p>
+              </div>
+            </CardContent>
+          </Card>
+        )}
+        {paymentsOfCurrentUser.map((payment) => (
+          <PaymentCard
+            user={user}
+            key={generateKey(payment.fromUserEmail)}
+            payment={payment}
+          />
+        ))}
+        {/* Users' balances */}
         {balances.map((balance) => (
           <BalanceCard
-            key={balance.userId}
+            key={generateKey(balance.userEmail)}
             email={balance.userEmail}
             balance={balance}
           />
         ))}
+        {/* Total expense */}
+        <Card className="max-w-md bg-gradient-to-br rounded-md">
+          <CardContent className="py-3">
+            <div className="flex items-center justify-between space-x-4">
+              <div className="p-3 text-slate-600">
+                <Receipt size={35} />
+              </div>
+              <div className="text-center">
+                <p className="text-sm text-gray-500 font-medium">
+                  Total Expense
+                </p>
+                <p className="text-xl font-semibold text-gray-700">
+                  {balancesObj?.totalExpenses?.toFixed(2) ?? '0.00'} €
+                </p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
