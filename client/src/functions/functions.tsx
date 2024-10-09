40e31469ae01dcf7227932b3db585fcb41b83483
Homvi <honvedoadamkj@gmail.com>
2024-10-09 09:11:34 +0200

    feat(functions): add getPayments function to retrieve expense group payments

--- client/src/functions/functions.tsx
+++ client/src/functions/functions.tsx
@@ -5,6 +5,7 @@ import { ExpenseGroup } from '../types/expenseGroup';
 import { Invitation } from '@/types/invitation';
 import { Balances } from '@/types/balance';
 import { Category } from '@/types/category';
+import { ExpenseGroupPayments } from '@/types/payment';
 
 export async function getExpenseGroups(
   userId: number
@@ -28,6 +29,12 @@ export async function getBalances(expenseGroupId: string): Promise<Balances> {
 
 export async function getExpenseGroup(id: string): Promise<ExpenseGroup> {
   const url = `${serverBaseUrl}/api/v1/expense-groups/${id}`;
+  const response = await fetch(url);
+  return response.json();
+}
+
+export async function getPayments(expenseGroupId: string): Promise<ExpenseGroupPayments> {
+  const url = `${serverBaseUrl}/api/v1/expense-groups/${expenseGroupId}/payments`;
   const response = await fetch(url);
   return response.json();
 }
